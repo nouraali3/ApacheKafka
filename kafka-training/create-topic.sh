0fb1a83fbd7eb6e325b054a4fe252d3c4dee7e95
nouraali3 <noura.ali9@yahoo.com>
2018-09-12 13:06:05 +0300

    kafka directory with windows guide

---
+++ kafka-training/create-topic.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+## Create topics
+
+"kafka/bin/kafka-topics.sh" --create --replication-factor 3 --partitions 13 --topic my-example-topic --zookeeper  localhost:2181
+
+## List created topics
+
+"kafka/bin/kafka-topics.sh" --list --zookeeper localhost:2181
